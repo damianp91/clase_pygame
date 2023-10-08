9344b32ad0a1d24b83d99c4a01e96b33d36338a7
Damian Posada <d4m14n36@gmail.com>
2023-10-08 04:15:49 -0300

    movimiento ok, ahora agregar diccionario

--- src/main.py
+++ src/main.py
@@ -1,33 +1,30 @@
 import pygame, sys
+from settings import *
 
-WIDTH = 600
-HEIGHT = 400
-center_x = WIDTH // 2
-center_y = HEIGHT // 2
-center_screen = (center_x, center_y)
+
 
 pygame.init() # Inicializar pygame
 
 clock = pygame.time.Clock()
-FPS = 60
 
 display = pygame.display.set_mode((WIDTH, HEIGHT)) # Tamaño de la pantalla recomendable sus valores como global asi no se use este comcepto en python 
 pygame.display.set_caption("Juego de prueba.")
-color = display.fill((78, 87, 146)) # Color a la pantalla principal
 
 #rect_1 = pygame.rect.Rect(100, 80, 200, 200) # Para dibujar un rectangulo, los dos primeros en desde donde comienza y lo otros el tamaño del rectan.
 #rect_1.center = center_screen
 #rect_2 = (300, 150, 120, 90) # lo mismo pero en tupla (construccion)
 
 # Movimiento 
-y = 20
-soil = HEIGHT - 20
 
 # Superficies
 #calco = pygame.Surface((100, 50))
 #calco.fill((0, 0, 255))
 #rec_calco = calco.get_rect()
 
+y = 300
+scuare = pygame.Rect(300, y, 80, 60)
+gravity_y = True
+gravity_x = True
 
 while True:
     
@@ -40,19 +37,37 @@ while True:
             pygame.quit() # Esta es la inversa de pygame.init aca avisamos que vamos a salir del programa
             sys.exit() # Al momento de dar x a la pantalla va a salir sin mostrar error
     
-    if(y <= soil):
-        y += 1
-    else:
-        y  
     
-    print(y)
+    if gravity_y:
+        if scuare.bottom <= HEIGHT:
+            scuare.top += SPEED
+        else:
+            gravity_y = not gravity_y
+    else:
+        if scuare.top >= 0:
+            scuare.top -= SPEED
+        else:
+            gravity_y = not gravity_y
+    
+    if gravity_x:
+        if scuare.right <= WIDTH:
+            scuare.right += SPEED
+        else:
+            gravity_x = not gravity_x
+    else:
+        if scuare.left >= 0:
+            scuare.left -= SPEED
+        else:
+            gravity_x = not gravity_x
+    
         # Esto es para ver figuras y como construirlas
         #-----> Actualiza los elementos
         #pygame.draw.rect(display, (255, 0, 0),rect_1, 3)
         #pygame.draw.rect(display, (0, 255, 0),rect_2)
     
-    display.fill((78, 87, 146))
-    pygame.draw.circle(display, (0, 255, 0), (center_x, y), 20)
+    display.fill(costume)
+    
+    pygame.draw.rect(display, green, scuare)
     
         #pygame.draw.line(display, (23, 189, 165), (0, 0), x.center, 3)
         #pygame.draw.ellipse(display, (89, 145, 111), (300, 200, 100, 150))
---
+++ src/settings.py
@@ -0,0 +1,25 @@
+
+# Screen
+WIDTH = 600
+HEIGHT = 400
+center_x = WIDTH // 2
+center_y = HEIGHT // 2
+center_screen = (center_x, center_y)
+
+
+# Motion speed
+FPS = 60
+
+
+# Speed 
+SPEED = 3 # velocidad por pixeles
+
+
+#colors
+green = (0, 255, 0)
+red = (255, 0, 0)
+black = (0, 0, 0)
+yellow = (255, 255, 0)
+white = (255, 255, 255)
+blue = (0, 0, 255)
+costume = (78, 87, 146)
